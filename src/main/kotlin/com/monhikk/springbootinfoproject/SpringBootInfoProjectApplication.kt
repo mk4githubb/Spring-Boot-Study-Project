4f3c79e259ba8c500dc4bb0129ff5d9850d929b5
Mony Kaushik <mony.kaushik@service.nsw.gov.au>
2022-03-08 19:24:48 +1100

    Initial project setup

---
+++ src/main/kotlin/com/monhikk/springbootinfoproject/SpringBootInfoProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.monhikk.springbootinfoproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootInfoProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootInfoProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/monhikk/springbootinfoproject/SpringBootInfoProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monhikk.springbootinfoproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootInfoProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
